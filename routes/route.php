bf09374e5ecdba2684094b50d1f155c943cca985
Salador <salador2005@yandex.ru>
2018-11-21 04:56:25 +0000

    Route update

--- routes/route.php
+++ routes/route.php
@@ -1,4 +1,11 @@
 <?php
+use Orchids\DemoKit\Http\Screens\Step1\DemokitStep1;
+use Orchids\DemoKit\Http\Screens\Step2\DemokitStep2Edit;
+use Orchids\DemoKit\Http\Screens\Step3\DemokitStep3;
+use Orchids\DemoKit\Http\Screens\Step3\DemokitStep3List;
+use Orchids\DemoKit\Http\Screens\Step4\DemokitStep4List;
+use Orchids\DemoKit\Http\Screens\Step5\DemokitStep5;
+
 
 
 Route::domain((string) config('platform.domain'))
@@ -6,16 +13,16 @@ Route::domain((string) config('platform.domain'))
     ->middleware(config('platform.middleware.private'))
     ->namespace('Orchids\DemoKit\Http\Screens')
     ->group(function (\Illuminate\Routing\Router $router, $path='platform.demokit.') {
-        $router->screen('step2/{postscreen}/edit', 'Step2\DemokitStep2Edit',$path.'step2.edit');
-        $router->screen('step2/create', 'Step2\DemokitStep2Edit',$path.'step2.create');
-        $router->screen('step4', 'Step4\DemokitStep4List',$path.'step4.list');
+        $router->screen('step2/{postscreen}/edit', DemokitStep2Edit::class)->name($path.'step2.edit');
+        $router->screen('step2/create', DemokitStep2Edit::class)->name($path.'step2.create');
+        $router->screen('step4', DemokitStep4List::class)->name($path.'step4.list');
 
-        $router->screen('step1', 'Step1\DemokitStep1',$path.'step1');
+        $router->screen('step1', DemokitStep1::class)->name($path.'step1');
 
-        $router->screen('step3/{step3}/column', 'Step3\DemokitStep3',$path.'step3');
-        $router->screen('step3', 'Step3\DemokitStep3List',$path.'step3.list');
+        $router->screen('step3/{step3}/column', DemokitStep3::class)->name($path.'step3');
+        $router->screen('step3', DemokitStep3List::class)->name($path.'step3.list');
 
-        $router->screen('step5', 'Step5\DemokitStep5',$path.'step5');
+        $router->screen('step5', DemokitStep5::class)->name($path.'step5');
 
     });
 
